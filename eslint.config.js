a81e7e4e54179025d1e7aa4a27f1a6dbe4f9ad7b
Coleman Irby <ckirbyee92@gmail.com>
2026-03-12 10:06:20 -0500

    initial push for CI integration, linting, and type checking

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  tseslint.configs.recommended,
+  { ignores: ["**/dist"] }
+);
